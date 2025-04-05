dc9ac0a38de19511f7cde7903e62d80b1d0654af
TyagiIshika <tyagiishika1701@gmail.com>
2025-04-05 18:17:08 +0530

    second commit

--- app/src/main/java/com/example/whatsapp/userregistrationscreen/UserRegistrationScreen.kt
+++ app/src/main/java/com/example/whatsapp/userregistrationscreen/UserRegistrationScreen.kt
@@ -1,15 +1,38 @@
 package com.example.whatsapp.userregistrationscreen
 
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,6 +43,20 @@ import com.example.whatsapp.R
 @Preview(showSystemUi = true)
 @Composable
 fun UserRegistrationScreen() {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    var selectedCountry by remember {
+        mutableStateOf("Japan")
+    }
+    var countryCode by remember {
+        mutableStateOf("+81")
+    }
+    var phoneNumber by remember {
+        mutableStateOf("")
+    }
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -29,9 +66,116 @@ fun UserRegistrationScreen() {
         Text(
             text = "Enter your phone number",
             fontSize = 20.sp,
-            color = colorResource(R.color.dark_green), fontWeight = FontWeight.Bold
+            color = colorResource(R.color.dark_green),
+            fontWeight = FontWeight.Bold
         )
-        Spacer(modifier = Modifier.height(23.dp))
+        Spacer(modifier = Modifier.height(24.dp))
+        Row {
+            Text(text = "WhatsApp will need to verify your phone number.")
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "What's", color = colorResource(R.color.dark_green))
+
+        }
+        Text(text = "my number?", color = colorResource(R.color.dark_green))
+        Spacer(modifier = Modifier.width(16.dp))
+        Box(
+            modifier = Modifier
+                .width(230.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            TextButton(onClick = { expanded = true }, modifier = Modifier.fillMaxWidth()) {
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = selectedCountry,
+                        modifier = Modifier.align(Alignment.Center),
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    )
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.CenterEnd),
+                        tint = colorResource(id = R.color.light_green)
+                    )
+                }
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                listOf("India", "USA", "China", "Canada","Australia").forEach { country ->
+                    DropdownMenuItem(text = { Text(text = country) }, onClick = {
+                        selectedCountry = country
+                        expanded = false
+                    })
+                }
+            }
+        }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(horizontal = 66.dp),
+            thickness = 2.dp,
+            color = colorResource(R.color.light_green)
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                TextField(
+                    value = countryCode,
+                    onValueChange = {
+                        countryCode = it
+                    },
+                    modifier = Modifier.width(70.dp),
+                    singleLine = true,
+                    textStyle = LocalTextStyle.current.copy(fontSize = 18.sp),
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedIndicatorColor = colorResource(R.color.light_green),
+                        focusedIndicatorColor = colorResource(R.color.light_green)
+                    )
+                )
+                Spacer(modifier = Modifier.width(6.dp))
+                TextField(
+                    value = phoneNumber,
+                    onValueChange = {
+                        phoneNumber = it
+                    },
+                    placeholder = { Text(text = "Phone Number") },
+                    singleLine = true,
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedIndicatorColor = colorResource(R.color.light_green),
+                        focusedIndicatorColor = colorResource(R.color.light_green)
+                    )
+                )
+
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Carrier charges may apply",
+                fontSize = 12.sp,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+            )
+            Spacer(modifier = Modifier.height(26.dp))
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(6.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.dark_green))
+            ) {
+                Text(text = "Next", fontSize = 16.sp)
+            }
+
+        }
+
 
     }
 }
