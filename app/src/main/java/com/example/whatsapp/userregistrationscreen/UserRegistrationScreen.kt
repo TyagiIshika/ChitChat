d5ba2f16407da3ebbe8fcbb397319d888f79678b
TyagiIshika <tyagiishika1701@gmail.com>
2025-04-05 12:00:25 +0530

    first commit

---
+++ app/src/main/java/com/example/whatsapp/userregistrationscreen/UserRegistrationScreen.kt
@@ -0,0 +1,37 @@
+package com.example.whatsapp.userregistrationscreen
+
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.whatsapp.R
+
+@Preview(showSystemUi = true)
+@Composable
+fun UserRegistrationScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Enter your phone number",
+            fontSize = 20.sp,
+            color = colorResource(R.color.dark_green), fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
+    }
+}
